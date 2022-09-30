30138ad4ce17db0c62a87e895fcacfaef5fa3bf1
Joshua <72043974+Joshuwat12@users.noreply.github.com>
2022-09-30 21:58:04 +0000

    First commit

--- count.py
+++ count.py
@@ -1,3 +1,23 @@
 # count.py
-#
-# 
+
+
+# Function definitions
+def count(string, chars):
+  total = 0
+  for letter in string:
+    if letter in chars:
+      total += 1
+  return total
+
+
+# Input
+string = input("Enter a string: ")
+chars = input("Enter a set of characters with no spaces: ")
+while ' ' in chars:
+  chars = input("I said no spaces! ")
+
+# Computation
+letters = count(string, chars)
+
+# Output
+print("The character(s) of", chars, "occur(s) in", string, letters, "times.")
